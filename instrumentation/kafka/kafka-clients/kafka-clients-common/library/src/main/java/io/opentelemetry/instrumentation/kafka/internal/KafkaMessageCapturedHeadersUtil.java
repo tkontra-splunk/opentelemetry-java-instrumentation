bed02dd862d20a6de7b4609837ee88cd5209b247
Tyler Kontra <tkontra@splunk.com>
2022-05-27 18:33:28 -0700

    KafkaCapturedMessageHeadersUtil: configuration interface

---
+++ instrumentation/kafka/kafka-clients/kafka-clients-common/library/src/main/java/io/opentelemetry/instrumentation/kafka/internal/KafkaMessageCapturedHeadersUtil.java
@@ -0,0 +1,58 @@
+package io.opentelemetry.instrumentation.kafka.internal;
+
+import static java.util.Collections.emptyList;
+import static java.util.Collections.unmodifiableList;
+
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.instrumentation.api.config.Config;
+import java.util.List;
+import java.util.Locale;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
+import java.util.stream.Collectors;
+
+public class KafkaMessageCapturedHeadersUtil {
+  private static final String CONSUMER_HEADERS_PROPERTY =
+      "otel.instrumentation.kafka.capture-headers.consumer";
+  private static final String PRODUCER_HEADERS_PROPERTY =
+      "otel.instrumentation.kafka.capture-headers.producer";
+
+  static final List<String> consumerMessageHeaders;
+  static final List<String> producerMessageHeaders;
+
+  static {
+    Config config = Config.get();
+    consumerMessageHeaders = config.getList(CONSUMER_HEADERS_PROPERTY, emptyList());
+    producerMessageHeaders = config.getList(PRODUCER_HEADERS_PROPERTY, emptyList());
+  }
+
+  // these are naturally bounded because they only store keys listed in
+  // otel.instrumentation.http.capture-headers.server.request and
+  // otel.instrumentation.http.capture-headers.server.response
+  private static final ConcurrentMap<String, AttributeKey<String>> consumerKeysCache =
+      new ConcurrentHashMap<>();
+  private static final ConcurrentMap<String, AttributeKey<String>> producerKeysCache =
+      new ConcurrentHashMap<>();
+
+  static List<String> lowercase(List<String> names) {
+    return unmodifiableList(
+        names.stream().map(s -> s.toLowerCase(Locale.ROOT)).collect(Collectors.toList()));
+  }
+
+  static AttributeKey<String> requestAttributeKey(String headerName) {
+    return consumerKeysCache.computeIfAbsent(headerName, n -> createKey("consumer", n));
+  }
+
+  static AttributeKey<String> responseAttributeKey(String headerName) {
+    return producerKeysCache.computeIfAbsent(headerName, n -> createKey("producer", n));
+  }
+
+  private static AttributeKey<String> createKey(String type, String headerName) {
+    // headerName is always lowercase, see CapturedHttpHeaders
+    String headerNormalized = headerName.replace('-', '_').replace(" ", "_");
+    String key = "kafka." + type + ".header." + headerNormalized;
+    return AttributeKey.stringKey(key);
+  }
+
+  private KafkaMessageCapturedHeadersUtil() {}
+}
